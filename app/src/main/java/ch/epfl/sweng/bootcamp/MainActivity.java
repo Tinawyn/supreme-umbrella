06059c03f5ad4a0336b9f247bf089277ad7fd790
Ivan Topcic <ivan.topcic@epfl.ch>
2018-09-25 16:00:09 +0200

    Fixed README.md

--- app/src/main/java/ch/epfl/sweng/bootcamp/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/bootcamp/MainActivity.java
@@ -1,7 +1,13 @@
 package ch.epfl.sweng.bootcamp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +16,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the  user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, GreetingActivity.class);
+        TextView textView = findViewById(R.id.greetingMessage);
+        String message = textView.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+
 }
